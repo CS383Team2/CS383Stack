05f4fba9476ebcb93d1899dec891423ef8a09a09
Nick K <n2143@hotmail.com>
2017-03-05 22:06:49 -0800

    RPS: Game working
    Added game result code

--- RockPaperScissors.cpp
+++ RockPaperScissors.cpp
@@ -4,11 +4,13 @@ using namespace std;
 
 #include "stack.h"
 
-const int noChoices = 4;
 #define Rock     1
 #define Paper    2
 #define Scissors 3
 #define Unknown  4
+#define Draw     5
+
+const int noChoices = 4;
 
 int whoWon(int p1, int p2);
 int parseResponce(char c);
@@ -25,7 +27,7 @@ void LetsPlayRockPaperScissors(){
     if (inChar != 'y')
         return;
     
-    
+    // Get player 1 choices
     cout << "\nRock \t\t1 or r" << endl;
     cout << "Paper \t\t2 or p" << endl;
     cout << "Scissors \t3 or s" << endl;
@@ -33,18 +35,17 @@ void LetsPlayRockPaperScissors(){
     for (i = 0; i < noChoices; i++) {
         cout << "Choice #" << i << ": ";
         cin >> inChar;
-        cout << endl;
         p1_choice = parseResponce(inChar);
         if (p1_choice == Unknown) {
            cout << "Unknown response. Exiting game" << endl; 
            return;
         }
         else {
-            stackP1.Push(p1_choice);
-            cout << "You choose: " << p1_choice << endl; 
+            stackP1.Push(p1_choice); 
         }
     }
     
+    // Get player 2 choices
     cout << "\nRock \t\t1 or r" << endl;
     cout << "Paper \t\t2 or p" << endl;
     cout << "Scissors \t3 or s" << endl;
@@ -53,7 +54,6 @@ void LetsPlayRockPaperScissors(){
     for (i = 0; i < noChoices; i++) {
         cout << "Choice #" << i << ": ";
         cin >> inChar;
-        cout << endl;
         p2_choice = parseResponce(inChar);
         if (p2_choice == Unknown) {
            cout << "Unknown response. Exiting game" << endl; 
@@ -61,16 +61,21 @@ void LetsPlayRockPaperScissors(){
         }
         else {
             stackP2.Push(p2_choice);
-            cout << "You choose: " << p2_choice << endl; 
         }
     }
     
+    // Calcuate results
+    cout << "\n\nRock \t\t1" << endl;
+    cout << "Paper \t\t2" << endl;
+    cout << "Scissors \t3" << endl;
     cout << "Let the best choices win!" << endl;
+    cout << "=========================" << endl;
     for (i = 0; i < noChoices; i++) {
         p1_choice = stackP1.Pop();
         p2_choice = stackP2.Pop();
         cout << "P1 choice: " << p1_choice <<
-                ". P2 choice: " << p2_choice <<endl;
+                ". P2 choice: " << p2_choice << " ";
+        whoWon(p1_choice, p2_choice);
     }
 }
 
@@ -83,6 +88,43 @@ int parseResponce(char c) {
         return Scissors;
     else
         return Unknown;
+}
+
+// Logic came from cs270 rock paper scissors assignment
+int whoWon(int p1, int p2) {
+    
+    char *temp, *word;
+    int modded, diff;    // for calculation p1 - p2
+    int wins, loses, tie;
+    
+    
+    // process logic of message
+    diff = p1 - p2; // player1 - player2
+    
+    //http://stackoverflow.com/questions/12276675/modulus-with-negative-numbers-in-c
+    modded = ((diff %= 3) < 0) ? diff + 3 : diff; // complicated because of neg number
+    
+    // RPS has a mathmatical relation to each other.
+    // test for conditions
+    if (p1 == p2){ // check for tie
+        tie++;
+        cout << "Result: Tie!" << endl;
+        return Draw;
+    }
+    else if (modded == 1){   // check for win
+        wins++;
+        cout << "Result: Player 1 wins!" << endl;
+        return 1;
+    }
+    else if (modded== 2){    // check for lose
+        loses++;
+        cout << "Result: Player 2 wins" << endl;
+        return 2;
+    }
+    else
+        cerr << "Incorrect data" << endl;
+    
+    return Unknown;
 }
 
 
