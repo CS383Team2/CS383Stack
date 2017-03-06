3defdcb0a70737d3d7308eb525f067dc105bca4b
Kevin Dorscher <kevindorscher@yahoo.com>
2017-03-05 17:15:36 -0800

    Branch for string reversal
    Hopefully, this is the correct way to branch, and edit a file in the group repo, so everyone can see it.
    
    I have changed the srting reversal to be done in main, and pass the array to a function to print it.
    This is probably not needed since the array elements of "reverse[]" will be printed one by one as soon as they are popped from the "iStack".
    
    I just added the print function to verify the pop order printed in main, and the array contents are the same.

--- main.cpp
+++ main.cpp
@@ -12,17 +12,23 @@ using namespace std;
 
 #include "stack.h"
 
+void reversePrint (int arr[]);
+
 main()
 {
+   reverse[5];
+   
    Stack iStack;
 
    cout << "Pushing integers onto iStack" << endl;
 
    for( int i = 0 ; i < 5 ; i++ ) {
       iStack.Push(i);       // push items onto the stack
+      reverse[i++] = i;     // Store values into the reverse array as they are added to the stack
       cout << i << ' ';
    }
    cout << endl;
+   i = 0;                   // Reset counter to fill array with reverse order.
    
    cout << "Contents of iStack" << endl;
    iStack.Print();          // output the stack contents
@@ -31,8 +37,8 @@ main()
    cout << endl << "Popping integers from iStack" << endl;
 
    while( !iStack.IsEmpty() )
-      cout << iStack.Pop() << ' ';
-
+      reverse[i++] = iStack.Pop();
+      
    cout << endl;
    iStack.Print();          // output the stack contents
 
@@ -41,6 +47,19 @@ main()
        cout << "The stack is empty" << endl;
    else
        cout << "The stack is not empty" << endl;
-
+   
+   reversePrint (&reverse);
+   
    return 0;
+}
+
+void reversePrint (int arr[])
+{
+   cout << "The reverse stack order is:" << endl;
+   int j;
+   for (j = 0; j < 5; j++)
+   {
+      cout << arr[j] << ' ';
+   }
+   cout << endl;
 }
