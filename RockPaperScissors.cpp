d0d1ab9bc9b93e25f9642186f5e24ae1196c9435
Nick K <n2143@hotmail.com>
2017-03-05 20:59:17 -0800

    RPS: Basic framework setup Created RockPaperScissors files Updated Makefile for new files Hooked into main()

---
+++ RockPaperScissors.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+#include "stack.h"
+
+int whoWon(int p1, int p2);
+
+int LetsPlayRockPaperScissors(){
+    Stack stackP1, stackP2;
+    string response;
+    
+    cout << "\n\nDo you want to play Rock Paper Scissors? (y/n): ";
+    cin >> response;
+    
+    
+    
+    
+}
+
+
+/* flow
+ * welcome
+ * p1 choices
+ * p2 choices
+ * 
+ * loop & test
+ * display winner
+ */
