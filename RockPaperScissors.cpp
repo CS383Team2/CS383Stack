86917cc52c1c8681b51118e504322164fe99f688
Nick K <n2143@hotmail.com>
2017-03-05 22:18:55 -0800

    RPS: Code cleanup
    Commented some code and cleaned up code

--- RockPaperScissors.cpp
+++ RockPaperScissors.cpp
@@ -4,26 +4,35 @@ using namespace std;
 
 #include "stack.h"
 
+/* Program flow
+ * welcome
+ * p1 choices
+ * p2 choices
+ * 
+ * loop & test
+ * display winner
+ */
+
 #define Rock     1
 #define Paper    2
 #define Scissors 3
 #define Unknown  4
 #define Draw     5
 
-const int noChoices = 4;
+const int noChoices = 5;
 
 int whoWon(int p1, int p2);
 int parseResponce(char c);
 
 void LetsPlayRockPaperScissors(){
     Stack stackP1, stackP2;
-    int i, j; //loop control
+    int i; //loop control
     int p1_choice, p2_choice;
     char inChar;
     
     cout << "\n\nDo you want to play Rock Paper Scissors? (y/n): ";
-    //cin >> response;
-    inChar = 'y'; // force y for development
+    cin >> inChar;
+    //inChar = 'y'; // force y for development
     if (inChar != 'y')
         return;
     
@@ -64,7 +73,7 @@ void LetsPlayRockPaperScissors(){
         }
     }
     
-    // Calcuate results
+    // calculate results
     cout << "\n\nRock \t\t1" << endl;
     cout << "Paper \t\t2" << endl;
     cout << "Scissors \t3" << endl;
@@ -79,6 +88,7 @@ void LetsPlayRockPaperScissors(){
     }
 }
 
+// Thought this would be longer :)
 int parseResponce(char c) {
     if (c == '1' || c == 'r')
         return Rock;
@@ -92,32 +102,26 @@ int parseResponce(char c) {
 
 // Logic came from cs270 rock paper scissors assignment
 int whoWon(int p1, int p2) {
-    
     char *temp, *word;
     int modded, diff;    // for calculation p1 - p2
-    int wins, loses, tie;
-    
     
     // process logic of message
+    // RPS has a mathematical relation to each other.
     diff = p1 - p2; // player1 - player2
     
     //http://stackoverflow.com/questions/12276675/modulus-with-negative-numbers-in-c
     modded = ((diff %= 3) < 0) ? diff + 3 : diff; // complicated because of neg number
     
-    // RPS has a mathmatical relation to each other.
     // test for conditions
     if (p1 == p2){ // check for tie
-        tie++;
         cout << "Result: Tie!" << endl;
         return Draw;
     }
     else if (modded == 1){   // check for win
-        wins++;
         cout << "Result: Player 1 wins!" << endl;
         return 1;
     }
     else if (modded== 2){    // check for lose
-        loses++;
         cout << "Result: Player 2 wins" << endl;
         return 2;
     }
@@ -126,13 +130,3 @@ int whoWon(int p1, int p2) {
     
     return Unknown;
 }
-
-
-/* flow
- * welcome
- * p1 choices
- * p2 choices
- * 
- * loop & test
- * display winner
- */
